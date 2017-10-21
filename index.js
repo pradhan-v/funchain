8252c3047e10af8c4e7514d5dcc6fcdba9b08cb9
Pradhan V <pradhan.v@pradhan-mac.local>
2017-10-21 23:39:15 +0530

    Added eslint, travis. Fix lint warnings.

--- index.js
+++ index.js
@@ -1,96 +1,95 @@
 'use strict';
 //
-var Chain = function(timeout, functionArray, callback) {
-	if (!(this instanceof Chain)) {
-		return new Chain(timeout, functionArray);
-	}
-	if (timeout) {
-		this.setTimeout(timeout);
-	}
-	if (functionArray) {
-		this.setFunctions(functionArray);
-	}
-	if (callback) {
-		this.setCallback(callback);
-	}
+const Chain = function Chain (timeout, functionArray, callback) {
+    if (!(this instanceof Chain)) {
+        return new Chain(timeout, functionArray);
+    }
+    if (timeout) {
+        this.setTimeout(timeout);
+    }
+    if (functionArray) {
+        this.setFunctions(functionArray);
+    }
+    if (callback) {
+        this.setCallback(callback);
+    }
 };
 //
-Chain.prototype.setTimeout = function(t) {
-	this._timeout = t;
+Chain.prototype.setTimeout = function (millis) {
+    this._timeout = millis;
 };
 //
-Chain.prototype.setFunctionArgs = function(args) {
-	this._functionArgs = args;
+Chain.prototype.setFunctionArgs = function (args) {
+    this._functionArgs = args;
 };
 //
-Chain.prototype.setFunctions = function(functionArray) {
-	this._functionArray = functionArray;
+Chain.prototype.setFunctions = function (functionArray) {
+    this._functionArray = functionArray;
 };
 //
-Chain.prototype.setCallback = function(callback) {
-	this._callback = callback;
+Chain.prototype.setCallback = function (callback) {
+    this._callback = callback;
 };
 //
-Chain.prototype.startCalls = function() {
-	if (arguments && arguments.length > 0) {
-		this.setFunctionArgs(Array.prototype.slice.call(arguments));
-	}
-	return callFunctions.apply(this, [this._timeout, this._functionArray].concat(this._functionArgs));
+Chain.prototype.startCalls = function (...args) {
+    if (args && args.length > 0) {
+        this.setFunctionArgs(args);
+    }
+    return this._callFunctions(...[this._timeout, this._functionArray].concat(this._functionArgs));
 };
 //
 // functionArguments will have the return value from the previous function call
 // and the original function arguments which was set or passed to startCalls function.
-const callFunctions = function(timeout, functions /*, functionArguments*/ ) {
-	//get arguments to the functiongs
-	let args = Array.prototype.slice.call(arguments, 2);
-	if (!functions || functions.length === 0) {
-		//if there are no functions to invoke then callback
-		if (this._callback) {
-			this._callback.apply(this._callback, args);
-		}
-		return;
-	}
-	let retvalue = null;
-	try {
-		//invoke the function
-		retvalue = functions[0].apply(functions[0], args);
-	} catch (err) {
-		if (this._callback) {
-			this._callback.apply(this._callback, err);
-		}
-		return;
-	}
-	//remove the invoked function from the array
-	var nextFunctions = functions.slice(1);
-	//check if return value is another chain
-	if (retvalue && retvalue instanceof Chain && retvalue._functionArray && retvalue._functionArray[0]) {
-		//push the new chain's functions to the beginning of the original chain
-		nextFunctions.unshift.apply(nextFunctions, retvalue._functionArray);
-		//replace function arguments
-		if (retvalue._functionArgs) {
-			retvalue = retvalue._functionArgs;
-		} else {
-			retvalue = args;
-		}
-	}
-	//prepare arguments for the next function call
-	let callargs = [timeout, nextFunctions];
-	//add the return value of the previous function first
-	if (retvalue) {
-		callargs = callargs.concat(retvalue);
-	}
-	//then add original arguments
-	if (this._functionArgs) {
-		callargs = callargs.concat(this._functionArgs);
-	}
-	var dhis = this;
-	if (timeout && timeout > 0) {
-		setTimeout(function() {
-			callFunctions.apply(dhis, callargs);
-		}, timeout);
-	} else {
-		callFunctions.apply(dhis, callargs);
-	}
+Chain.prototype._callFunctions = function (timeout, functions, ...args) {
+    // get arguments to the functiongs
+    if (!functions || functions.length === 0) {
+        // if there are no functions to invoke then callback
+        if (this._callback) {
+            this._callback.apply(this._callback, args);
+        }
+        return;
+    }
+    let retvalue = null;
+    try {
+        // invoke the function
+        retvalue = functions[0].apply(functions[0], args);
+    } catch (err) {
+        if (this._callback) {
+            this._callback.apply(this._callback, err);
+        }
+        return;
+    }
+    // remove the invoked function from the array
+    const nextFunctions = functions.slice(1);
+    // check if return value is another chain
+    if (retvalue && retvalue instanceof Chain && retvalue._functionArray && retvalue._functionArray[0]) {
+        // push the new chain's functions to the beginning of the original chain
+        nextFunctions.unshift(...retvalue._functionArray);
+        // replace function arguments
+        if (retvalue._functionArgs) {
+            retvalue = retvalue._functionArgs;
+        } else {
+            retvalue = args;
+        }
+    }
+    // prepare arguments for the next function call
+    let callargs = [timeout, nextFunctions];
+    // add the return value of the previous function first
+    if (retvalue) {
+        callargs = callargs.concat(retvalue);
+    }
+    // then add original arguments
+    if (this._functionArgs) {
+        callargs = callargs.concat(this._functionArgs);
+    }
+    const that = this;
+    if (timeout && timeout > 0) {
+        setTimeout(() => {
+            that._callFunctions(...callargs);
+        }, timeout);
+    } else {
+        that._callFunctions(...callargs);
+    }
 };
 //
 module.exports = Chain;
--- test/test.js
+++ test/test.js
@@ -1,55 +1,61 @@
 'use strict';
-const expect = require('chai').expect;
+const {expect} = require('chai');
 const mocha = require('mocha');
-const describe = mocha.describe;
-const it = mocha.it;
+const {describe} = mocha;
+const {it} = mocha;
 const Chain = require('../index');
+const delay = 100;
 //
-const addOne = (n) => n + 1;
-//appends a new character to the string. the char code of this new charater is 1+char code of the last character in the string (for abc, we get abcd, c+1)
-const addNextChar = (s) => s + String.fromCharCode(s.charCodeAt(s.length - 1) + 1);
+const addOne = (num) => num + 1;
+// appends a new character to the string. the char code of this new charater is
+// 1+char code of the last character in the string (for abc, we get abcd, c+1)
+const addNextChar = (str) => str + String.fromCharCode(str.charCodeAt(str.length - 1) + 1);
 //
-describe('#chain', function() {
-	it('check order', function(done) {
-		let f = new Chain(100, [addNextChar, addNextChar, addNextChar], (f) => {
-			expect(f).to.equal('abcd');
-			done();
-		});
-		f.startCalls('a');
-	});
-	it('check return values', function(done) {
-		let f = new Chain(100, [addOne, addOne, addOne], (f) => {
-			expect(f).to.equal(4);
-			done();
-		});
-		f.startCalls(1);
-	});
-	it('check arguments list', function(done) {
-		let f = new Chain(100, [(n, c) => {
-			expect(n).to.equal(1);
-			expect(c).to.equal('a');
-			return 123;
-		}, (r, n, c) => {
-			expect(r).to.equal(123);
-			expect(n).to.equal(1);
-			expect(c).to.equal('a');
-			return 456;
-		}, (r, n, c) => {console.log("rnc", r,n,c);
-			expect(r).to.equal(456);
-			expect(n).to.equal(1);
-			expect(c).to.equal('a');
-			return 789;
-		}], (f) => {
-			expect(f).to.equal(789);
-			done();
-		});
-		f.startCalls(1, 'a');
-	});
-	it('check no return values', function(done) {
-		let f = new Chain(100, [(n) => n + 1, (n) => {}, () => 100], (f) => {
-			expect(f).to.equal(100);
-			done();
-		});
-		f.startCalls(1);
-	});
+/* eslint no-magic-numbers:0 */
+describe('#chain', () => {
+    it('check order', (done) => {
+        const fchain = new Chain(delay, [addNextChar, addNextChar, addNextChar], (finalVal) => {
+            expect(finalVal).to.equal('abcd');
+            done();
+        });
+        fchain.startCalls('a');
+    });
+    it('check return values', (done) => {
+        const fchain = new Chain(delay, [addOne, addOne, addOne], (finalVal) => {
+            expect(finalVal).to.equal(4);
+            done();
+        });
+        fchain.startCalls(1);
+    });
+    it('check arguments list', (done) => {
+        const fchain = new Chain(delay, [
+            (num, chr) => {
+                expect(num).to.equal(1);
+                expect(chr).to.equal('a');
+                return 123;
+            }, (retVal, num, chr) => {
+                expect(retVal).to.equal(123);
+                expect(num).to.equal(1);
+                expect(chr).to.equal('a');
+                return 456;
+            }, (retVal, num, chr) => {
+                expect(retVal).to.equal(456);
+                expect(num).to.equal(1);
+                expect(chr).to.equal('a');
+                return 789;
+            }
+        ], (finalVal) => {
+            expect(finalVal).to.equal(789);
+            done();
+        });
+        fchain.startCalls(1, 'a');
+    });
+    it('check no return values', (done) => {
+        /* eslint no-empty-function:0 */
+        const fchain = new Chain(delay, [addOne, () => {}, () => 111], (finalVal) => {
+            expect(finalVal).to.equal(111);
+            done();
+        });
+        fchain.startCalls(1);
+    });
 });
